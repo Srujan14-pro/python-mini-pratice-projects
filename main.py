d1a747c55e3e9f870fb4ff1b096827769050bebf
Nagavivek-2010 <121575820+Nagavivek-2010@users.noreply.github.com>
2026-08-31 00:09:48 +0530

    more programs

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-# a=float(input("enter num:"))
-# b=float(input("enter num:"))
-# sums=a+b
-# sub=a-b
-# mul=a*b
-# div=a/b
-# mod=a%b
-#
-# print(f"the add of {a} and {b} is {sums}")
-# print(f"the sub of {a} and {b} is {sub}")
-# print(f"the mul of {a} and {b} is {mul}")
-# print(f"the div of {a} and {b} is {div}")
-# print(f"the mod of {a} and {b} is {mod}")
+a=float(input("enter num:"))
+b=float(input("enter num:"))
 
+
+print("1.+")
+print("2.-")
+print("3.*")
+print("4./")
+print("5.%")
+
+choice=int(input("Enter your choice:"))
+if choice==1:
+    print(f"the add of {a} and {b} is ",a+b)
+elif choice==2:
+    print(f"the sub of {a} and {b} is",a-b)
+elif choice==3:
+    print(f"the mul of {a} and {b} is ",a*b)
+elif choice==4:
+    print(f"the div of {a} and {b} is ",a/b)
+elif choice==5:
+    print(f"the mod of {a} and {b} is ",a%b)
 
