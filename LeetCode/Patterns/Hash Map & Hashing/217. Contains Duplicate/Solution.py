8fd636618d0fab49c3a9ab43bda1ba5bf6f1f1a8
Srujan14-pro <nagasrujan89@gmail.com>
2026-09-02 22:42:35 +0530

    Add Easy Python Solution for 217. Contains Duplicate from LeetCode

---
+++ LeetCode/Patterns/Hash Map & Hashing/217. Contains Duplicate/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for num in nums:
+            if nums.count(num)>=2:
+                return True
+            return False
+        
