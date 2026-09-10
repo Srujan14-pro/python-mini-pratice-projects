83866dbe7968398b10c6f0d66a8982b5d8f45e90
Srujan <nagasrujan89@gmail.com>
2026-09-11 00:23:51 +0530

    Add Easy Python Solution for 1365. How Many Numbers Are Smaller Than the Current Number from LeetCode

---
+++ LeetCode/Patterns/Hash Map & Hashing/1365. How Many Numbers Are Smaller Than the Current Number/Solution.py
@@ -0,0 +1,29 @@
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+#         temp=sorted((nums))
+#         d={}
+        
+#         for i,num in enumerate(temp):
+#             if i not in nums:
+#                 d[num]=i
+#         ret=[]
+#         for num in nums:
+#             ret.append(d[num])
+#         return ret
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+
+        temp = sorted(nums)
+
+        d = {}
+        ret = []
+
+        for i, num in enumerate(temp):
+            if num not in d:
+                d[num] = i
+
+        for num in nums:
+            ret.append(d[num])
+
+        return ret
+        
