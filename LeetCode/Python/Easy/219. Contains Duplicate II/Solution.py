70ed8ec8eb37da19578a1324d74f29f6037adbec
Srujan <nagasrujan89@gmail.com>
2026-09-13 23:01:57 +0530

    Add Easy Python Solution for 219. Contains Duplicate II from LeetCode

--- LeetCode/Python/Easy/219. Contains Duplicate II/Solution.py
+++ LeetCode/Python/Easy/219. Contains Duplicate II/Solution.py
@@ -1,9 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        dup={}
-        for i,num in enumerate(nums):
-            if num in dup:
-                if i-dup[num]<=k:
-                    return True
-            dup[num]=i
-        return False
+
+        for i in range(len(nums)):
+            for j in range(i+1,nums):
+                if nums[i]==nums[j] and 
+        # dup={}
+        # for i,num in enumerate(nums):
+        #     if num in dup:
+        #         if i-dup[num]<=k:
+        #             return True
+        #     dup[num]=i
+        # return False
