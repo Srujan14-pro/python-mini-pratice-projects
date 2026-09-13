c78e630030d50b2db8b0722cd7aa370ae46c3751
Srujan <nagasrujan89@gmail.com>
2026-09-13 23:01:59 +0530

    Add Easy Python Solution for 219. Contains Duplicate II from LeetCode

--- LeetCode/Patterns/Sliding Window/219. Contains Duplicate II/Solution.py
+++ LeetCode/Patterns/Sliding Window/219. Contains Duplicate II/Solution.py
@@ -3,7 +3,7 @@ class Solution:
 
         for i in range(len(nums)):
             for j in range(i+1,nums):
-                if nums[i]==nums[j] and 
+                if nums[i]==nums[j] and abs(i - j) <= k
         # dup={}
         # for i,num in enumerate(nums):
         #     if num in dup:
